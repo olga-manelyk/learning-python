709833d78a01657e2c0fbbb86cb79f1461001ce3
Olga Manelyk <olga.manelyk@gmail.com>
2024-05-07 20:33:09 +0300

    fix task 5-10

--- chapter 5/5-10.py
+++ chapter 5/5-10.py
@@ -1,10 +1,10 @@
 current_users = ["olga", "anna", "viktor", "denus", "pavlo", "admin", "ivan"]
-new_users = ["anna", "viktor", "denus", "pavlo", "ivan"]
+new_users = ["john", "viktor", "denus", "biba", "boba"]
 
-for new_users in current_users:
-    if new_users in current_users:
-        print("Sorry, username " + new_users + " is not available!")
+for new_user in new_users:
+    if new_user in current_users:
+        print("Sorry, username " + new_user + " is not available!")
     else:
-        print("Adding " + new_users + ".")
+        print("Adding " + new_user + ".")
 
 print("\nThank you, colleagues!")
